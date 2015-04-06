42907ff4b9dc461d71b619f7954446f5b77b2685
Kristofer Lindberg <k.lindberg@gmail.com>
2015-04-06 18:02:45 +0200

    Make conf work with lower versions of Love

--- conf.lua
+++ conf.lua
@@ -1,7 +1,11 @@
 function love.conf(t)
+	t.window = t.window or t.screen
+
 	t.title = "Fibonacci"
-	t.window.width = 800
-	t.window.height = 600
+	t.window.width = 1024
+	t.window.height = 768
 
 	t.console = true
+
+	t.screen = t.screen or t.window
 end
